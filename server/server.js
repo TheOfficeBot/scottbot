2154490322007134bae8da0840bd62bf6a827e0c
Patrick Iwanicki <patrickiwanicki@Patricks-MacBook-Pro.local>
2016-12-04 08:16:59 -0800

    update server for prod env

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ const app = express();
 app.use( morgan('dev') );
 app.use( bodyParser.json() );
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use('/',express.static('client/build'));
+} else {
+   app.use('/', express.static(path.join(__dirname, 'public')));
+}
+
 
 
 
