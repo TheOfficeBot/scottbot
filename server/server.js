0713d19374c7d3fa70cbeebb13e5d4c9274c0c0c
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2016-12-06 12:08:22 -0800

    console.log change

--- server/server.js
+++ server/server.js
@@ -29,6 +29,7 @@ app.get('/api/content', contentController.get);
 app.post('/api/addcontent', contentController.post);
 app.post('/api/slack', slackAPI.post);
 app.get('/slack', function(req, res){
+	console.log(req.query);
 	if (!req.query.code) {
         res.status(500);
         res.send({"Error": "Looks like we're not getting code."});
