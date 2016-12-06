46cd5ed64e2b94f66c4c3d625d16adb11420593b
Jackie Feiler <your_jacqueline.elana@gmail.com>
2016-12-05 18:46:49 -0800

    [fix] unstaged file added

--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
 const express 		= require('express'),
+	   app = express(),
 	    morgan 		= require('morgan'),
 	    path      	= require('path'),
-	    bodyParser = require('body-parser')
+	    bodyParser = require('body-parser');
 	    // Router = require('./controllers/****'); // ADD WHEN DONE
 var contentController = require('./controllers/content.js')
-var db = require('./db/config.js')
+var db = require('./db/config.js');
+var slackAPI = require('./controllers/slackapi.js');
 
 // CONFIG (USE) ============================
-
-const app = express();
-
 app.use( morgan('dev') );
 app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-
+if (process.env.NODE_ENV === 'production') {
+  app.use('/',express.static('client/build'));
+} else {
+   app.use('/', express.static(path.join(__dirname, 'public')));
+}
 
 // LISTEN (SET) =============================
-
 app.set('port', (process.env.PORT || 3001));
-
 app.listen(app.get('port'), function(){
-	console.log('API Server started: http://localhost:' + app.get('port') + '/');	
+	console.log('API Server started: http://localhost:' + app.get('port') + '/');
 })
-
 // ROUTING (GET) =============================
-app.get('/api/content', contentController.get)
-app.post('/api/addcontent', contentController.post)
+app.get('/api/content', contentController.get);
+app.post('/api/addcontent', contentController.post);
+
+app.post('/api/slack', slackAPI.post);
 // Connect controller for endpoint
 //app.use('/api/tasks', taskRouter)
 
 app.get('*', (req, res) => {
 	res.sendfile('./client/public/index.html');
 })
-
