0bf4a7992f9bbf17277192c1602da31d8df1a9e4
Jackie Feiler <your_jacqueline.elana@gmail.com>
2016-12-05 19:17:53 -0800

    [fix] merge conflict

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const auth = new AuthService("HpthymkekKBCqwn5w03aWCTvi6taPMb3", 'scottbot.auth0
 import Activity from './components/Activity';
 import Admin from './components/Administrator';
 import { Router, Route, browserHistory } from 'react-router';
-import './index.css';
+// import './index.css';
 import AuthService from './helpers/AuthService.js';
 import Login from './components/Login.js';
 import Container from './components/Container.js';
@@ -31,11 +31,11 @@ const requireAuth = (nextState, replace) => {
 
 const Routes = (
 	 <Router history={browserHistory}>
-	    <Route path="/" component={Container} auth={auth}>
-	    	<Route path="/home" component={App}/>
-	    	<Route path="/login"  component={Login}/>
+	    
+	    	<Route path="/" component={App}/>
+	    	<Route path="/login" auth={auth} component={Login}/>
 	    	<Route path="/admin" component={Admin} onEnter={requireAuth}/>
-	    </Route>
+	  
   	</Router>
 )
 >>>>>>> [fix] merge conflict fix
